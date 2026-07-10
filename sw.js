c083db82b8f72847e56754c6d4ab23de51a7deb6
bombyul1011 <faith_003@naver.com>
2026-07-10 20:48:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260711-22-icon-fixes-orb-size';
+const CACHE = 'iikoto-v20260711-25-forest-card-reorder';
 const ASSETS = [
   './',
   './index.html'
