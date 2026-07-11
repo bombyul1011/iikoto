b14dbd3037687481d725b446c29cf760ce4993ab
bombyul1011 <faith_003@naver.com>
2026-07-11 19:59:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260711-73-rhythm-banner-color';
+const CACHE = 'iikoto-v20260711-74-rhythm-rainbow-icon';
 const ASSETS = [
   './',
   './index.html'
