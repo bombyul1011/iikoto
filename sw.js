d1c6250831fd18b8b4fa29c8526b382f21f7a621
bombyul1011 <faith_003@naver.com>
2026-07-11 19:18:11 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260711-68-rc-arrow-dot-white-start';
+const CACHE = 'iikoto-v20260711-69-fixed-bg-layer-glass-icon';
 const ASSETS = [
   './',
   './index.html'
