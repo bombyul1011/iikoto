2680b63bde6d37a96e1b3ad8c1a11b4a1a3f3c6c
bombyul1011 <faith_003@naver.com>
2026-07-11 15:05:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260711-51-pace-bar-total-ratio-final';
+const CACHE = 'iikoto-v20260711-53-pace-card-refresh-fix';
 const ASSETS = [
   './',
   './index.html'
