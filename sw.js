8e59d7f36c85bd47f9e2870679f3e706ee342f6b
bombyul1011 <faith_003@naver.com>
2026-07-12 23:23:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260712-19-habit-check-textcard-tone';
+const CACHE = 'iikoto-v20260712-20-habit-check-theme-split';
 const ASSETS = [
   './',
   './index.html'
