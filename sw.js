f914027147227fa9cb6d32594bc1026f4b3b9d53
bombyul1011 <faith_003@naver.com>
2026-07-12 19:31:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260712-16-monday-challenge-review';
+const CACHE = 'iikoto-v20260712-17-rhythm-cat-guide';
 const ASSETS = [
   './',
   './index.html'
