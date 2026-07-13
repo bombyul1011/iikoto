a43b2d025d38c7cbffe1cf77a391e84d99ccfa2d
bombyul1011 <faith_003@naver.com>
2026-07-13 15:50:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260713-25-morning-habit-wchallenge-sync-fix';
+const CACHE = 'iikoto-v20260713-26-wed-report-removed-kr-labels';
 const ASSETS = [
   './',
   './index.html'
