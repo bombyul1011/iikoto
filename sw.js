fd8eab428152ac94115f34d976ae63ea4deda673
bombyul1011 <faith_003@naver.com>
2026-07-14 14:12:18 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260714-39-tab-swipe-fade-slide-anim';
+const CACHE = 'iikoto-v20260714-40-todo-longpress-reorder';
 const ASSETS = [
   './',
   './index.html'
