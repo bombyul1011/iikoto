75decd04e6a1c5ed7ddd027b8ecff495c68826ad
bombyul1011 <faith_003@naver.com>
2026-07-17 18:31:22 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260717-105-playlist-sheet-overlap-fix';
+const CACHE = 'iikoto-v20260717-106-content-sort-playlist-color';
 const ASSETS = [
   './',
   './index.html'
