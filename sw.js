9c467e5ec666edd7f2d3ef32db2abdde86749cf2
bombyul1011 <faith_003@naver.com>
2026-07-18 01:58:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260718-2-initial-load-sync';
+const CACHE = 'iikoto-v20260718-4-todo-timeline-simple';
 const ASSETS = [
   './',
   './index.html'
