152f96199488e271682570d44593480e7acdc535
bombyul1011 <faith_003@naver.com>
2026-07-18 22:58:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260718-21-sleep-badge-restyle';
+const CACHE = 'iikoto-v20260718-22-sleep-badge-size-down';
 const ASSETS = [
   './',
   './index.html'
