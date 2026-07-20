c2456418ae950b86df254e5fbaf1f99059e3c0e4
bombyul1011 <faith_003@naver.com>
2026-07-20 20:48:08 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260720-13-elearn-mustard-olive';
+const CACHE = 'iikoto-v20260720-15-elearn-olive-gold-blend';
 const ASSETS = [
   './',
   './index.html'
