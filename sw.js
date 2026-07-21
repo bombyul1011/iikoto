ca358fa098fdede15933a4e248902a4b83fe1ca3
bombyul1011 <faith_003@naver.com>
2026-07-21 16:41:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260721-14-chaeum-prompt-fix';
+const CACHE = 'iikoto-v20260721-15-chaeum-glass-theme';
 const ASSETS = [
   './',
   './index.html'
