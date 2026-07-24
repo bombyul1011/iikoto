3ce732c2a89358fbbf87832f50164a8bc1603e5d
bombyul1011 <faith_003@naver.com>
2026-07-24 23:42:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260724-9-greeting-prompt-dedup';
+const CACHE = 'iikoto-v20260724-10-rhythm-color-tuning';
 const ASSETS = [
   './',
   './index.html'
