4977400ded3294b20f9a4986a5dc4cc46050d307
bombyul1011 <faith_003@naver.com>
2026-07-24 19:12:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260724-5-reading-duration-label';
+const CACHE = 'iikoto-v20260724-6-weekly-goal-creative';
 const ASSETS = [
   './',
   './index.html'
