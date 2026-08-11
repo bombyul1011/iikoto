0d1b206dba61f620650a1d84c2dca119c2414af5
bombyul1011 <faith_003@naver.com>
2026-08-11 19:35:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260810-09-chevron-icon-fix';
+const CACHE = 'iikoto-v20260811-02-reminder-popup-test';
 const ASSETS = [
   './',
   './index.html'
