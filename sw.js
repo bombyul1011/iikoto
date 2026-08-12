30615875e81c056c455f825b3b0faa9268a58f12
bombyul1011 <faith_003@naver.com>
2026-08-12 23:35:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260812-11-chaeum-topic-tree';
+const CACHE = 'iikoto-v20260812-23-today-card-remove-session';
 const ASSETS = [
   './',
   './index.html'
