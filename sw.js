cb0c935cb5aba46838ce19d8ecb4c75ca19e48c0
bombyul1011 <faith_003@naver.com>
2026-08-13 23:11:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // iikoto Service Worker
-const CACHE = 'iikoto-v20260813-29-moneybag-heart-center-align';
+const CACHE = 'iikoto-v20260813-30-confetti-schedule-modal-zorder';
 const ASSETS = [
   './',
   './index.html'
